1ebbf45cd8f5c4ae1bf02a4fa5f41c104350f01f
Joram <jomwashighadi@gmail.com>
2021-02-15 20:10:42 +0300

    feat: add ch name to user

--- src/db/models/User.ts
+++ src/db/models/User.ts
@@ -16,6 +16,7 @@ const userSchema = new mongoose.Schema({
   firstName: { type: String },
   lastName: { type: String },
   dateOfBirth: { type: Date },
+  chvName: { type: String },
   phoneNumber: {
     type: String,
     required: true,
